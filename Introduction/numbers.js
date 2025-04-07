682149d6f9f1d6556f0fbbb8dbed9317fbe277be
r0elcompute <rodgersmwangi20@gmail.com>
2025-04-07 11:44:42 +0300

    Added the functions folder

---
+++ Introduction/numbers.js
@@ -0,0 +1,15 @@
+console.log((3 + 2) - 76 * (1 + 1));
+
+//the Number() constructor
+
+let myNumber = "74";
+//re assign using the Number() constructor
+
+myNumber = Number(myNumber) + 3;
+console.log(myNumber);
+
+//a ternary operator 
+const birthday  = true;
+let neighbor = birthday
+ ? "that is true" 
+ :"that is false";
---
+++ Introduction/variables.js
@@ -0,0 +1,27 @@
+
+//we use let and const or sometimes var to declare variables
+
+//variables declared with the let keyword can be reassigned!
+
+// var as a keyword for declaring variables is not being used anymore but it is still useful to know it exists
+let name = "Rodgers";
+let surname = "Mwangi";
+
+console.log(name);
+console.log(surname);
+
+//re assign variables
+let age = 11;
+//concatenatipn - directly adding two or more values to produce one outcome
+console.log("my age was " + age + " years old back in 2016");
+
+age = 20;
+
+console.log("I am now " + age + " years old"); //concatenation
+
+//variables declared with the const keyword cannot be re assigned!
+
+const pi = 3.142;
+// pi = 20.2;
+console.log("The value for mathematical pi is " + pi);
+
