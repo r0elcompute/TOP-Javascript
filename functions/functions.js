5c220b412bd13d78a853150217b067b4af285271
r0elcompute <rodgersmwangi20@gmail.com>
2025-04-07 11:42:44 +0300

    Added the functions exercises folder

---
+++ functions/functions.js
@@ -0,0 +1,43 @@
+//This is a code snippet showcasing a function
+function myAnimal (animal) {
+    return animal + " is my favorite animal";
+}
+
+console.log(myAnimal("Goat"));
+
+//second function to add a number to 7
+function add7 (number) {
+    return number + 7;
+}
+
+console.log(add7(7));
+
+//third function that multiplies two numbers 
+function multiply (number1, number2) {
+    return number1 * number2;
+}
+
+console.log(multiply(10, 10));
+
+//function to return last letter of string
+function lastLetter (Rodgers) {
+    return 
+}
+
+
+//function to capitalize the first letter of a string
+function capitalize (string) {
+    return 
+}
+
+//THOSE ABOVE HERE WERE USER DEFINED FUNCTIONS HERE ARE INBUILT BROWSER FUNCTIONS
+
+const myNumber = Math.random();//math.random() generates random numbers that are decimals less than 1
+console.log(myNumber);
+
+const myArray = ["Rodgers", "Fanuel", "Mwangi"];
+console.log(myArray.join());//<arrayName>.join() joins the items of an array into a single string
+
+const myString = "I am a string";
+const stringReplacement = myString.replace("string", "STRING");//<stringName>.replace() replaces a string with another
+console.log(stringReplacement);
